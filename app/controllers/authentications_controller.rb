b69eddd9251cfac40f475913fa7856bce949a5f5
James Stewart <jys@ketlai.co.uk>
2012-01-17 12:16:02 +0000

    Provide sign out action via gds-sso

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -3,4 +3,11 @@ class AuthenticationsController < ApplicationController
   def callback
     redirect_to session["return_to"] || '/'
   end
+
+  def sign_out
+    cookie_key = Rails.application.config.session_options[:key]
+    cookies.delete(cookie_key)
+    reset_session
+    redirect_to Plek.current.find('signonotron') + "/users/sign_out"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   match '/auth/gds/callback', :to => 'authentications#callback', :as => 'gds_login_path'
+  match '/auth/gds/sign_out', to: 'authentications#sign_out', as: :gds_sign_out
 end
