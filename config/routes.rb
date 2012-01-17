577e4951b5c1686c5d4fa0abd3ffa233ebed8319
James Stewart <jys@ketlai.co.uk>
2012-01-17 12:16:28 +0000

    Rename 'gds_login_path_path' to 'gds_sign_in_path'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  match '/auth/gds/callback', :to => 'authentications#callback', :as => 'gds_login_path'
+  match '/auth/gds/callback', to: 'authentications#callback', as: :gds_sign_in
   match '/auth/gds/sign_out', to: 'authentications#sign_out', as: :gds_sign_out
 end
