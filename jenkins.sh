3d445512089735b6792036b63dd405830f1c4bcf
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-04-19 07:41:50 -0700

    Run integration specs.

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -x
+
+# Make sure this runs, even if something blows up.
+trap "bundle exec rake signonotron:stop" EXIT
+
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 bundle exec rake test
+bundle exec rake signonotron:start spec
