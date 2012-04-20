1ba5417376ed94135cf348ee0715840dea558d79
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-04-20 04:57:54 -0700

    Spec around a pre-authorised client application

--- spec/requests/end_to_end_spec.rb
+++ spec/requests/end_to_end_spec.rb
@@ -6,30 +6,62 @@ describe "Integration of client using GDS-SSO with signonotron" do
   before :all do
     wait_for_signonotron_to_start
   end
-
   before :each do
     @client_host = 'www.example-client.com'
     Capybara.current_driver = :mechanize
     Capybara::Mechanize.local_hosts << @client_host
-    page.driver.header 'accept', 'text/html'
 
     load_signonotron_fixture
   end
 
-  specify "accessing a non-restricted page" do
-    visit "http://#{@client_host}/"
-    page.should have_content('jabberwocky')
+  describe "Web client accesses" do
+    before :each do
+      page.driver.header 'accept', 'text/html'
+    end
+
+    specify "a non-restricted page can be accessed without authentication" do
+      visit "http://#{@client_host}/"
+      page.should have_content('jabberwocky')
+    end
+
+    specify "first access to a restricted page requires authentication and application approval" do
+      visit "http://#{@client_host}/restricted"
+      page.should have_content("Sign in")
+      fill_in "Email", :with => "test@example-client.com"
+      fill_in "Password", :with => "q1w2e3r4t5y6u7i8o9p0"
+      click_on "Sign in"
+
+      click_on "Authorize"
+
+      page.should have_content('restricted kablooie')
+    end
+
+    specify "access to a restricted page for an approved application requires only authentication" do
+      # First we login to authorise the app
+      visit "http://#{@client_host}/restricted"
+      fill_in "Email", :with => "test@example-client.com"
+      fill_in "Password", :with => "q1w2e3r4t5y6u7i8o9p0"
+      click_on "Sign in"
+      click_on "Authorize"
+
+      # At this point the app should be authorised, we reset the session to simulate a new browser visit.
+      reset_session!
+      page.driver.header 'accept', 'text/html'
+
+      visit "http://#{@client_host}/restricted"
+      page.should have_content("Sign in")
+      fill_in "Email", :with => "test@example-client.com"
+      fill_in "Password", :with => "q1w2e3r4t5y6u7i8o9p0"
+      click_on "Sign in"
+
+      page.should have_content('restricted kablooie')
+    end
   end
 
-  specify "accessing a restricted page, and logging in successfully" do
-    visit "http://#{@client_host}/restricted"
-    page.should have_content("Sign in")
-    fill_in "Email", :with => "test@example-client.com"
-    fill_in "Password", :with => "q1w2e3r4t5y6u7i8o9p0"
-    click_on "Sign in"
+  describe "API client accesses" do
+    before :each do
+      page.driver.header 'accept', 'application/json'
+    end
 
-    click_on "Authorize"
-
-    page.should have_content('restricted kablooie')
   end
 end
