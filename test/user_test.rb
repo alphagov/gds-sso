51b9363ba7d49d4fbe99b89597ad973264a6fb0a
Mazz <mazz.mosley@digital.cabinet-office.gov.uk>
2012-06-12 16:04:50 +0100

    Remove unused hash values.

--- test/user_test.rb
+++ test/user_test.rb
@@ -8,12 +8,12 @@ class TestUser < Test::Unit::TestCase
       'uid' => 'abcde',
       'credentials' => {'token' => 'abcdefg', 'secret' => 'abcdefg'},
       'info' => {'name' => 'Matt Patterson', 'email' => 'matt@alphagov.co.uk'},
-      'extra' => {'user_hash' => {'uid' => 'abcde', 'version' => 1, 'name' => 'Matt Patterson', 'email' => 'matt@alphagov.co.uk', 'github' => 'fidothe', 'twitter' => 'fidothe'}}
+      'extra' => {'user' => {'uid' => 'abcde', 'name' => 'Matt Patterson', 'email' => 'matt@alphagov.co.uk'}}
     }
   end
 
   def test_user_params_creation
-    expected = {'uid' => 'abcde', 'version' => 1, 'name' => 'Matt Patterson', 'email' => 'matt@alphagov.co.uk'}
+    expected = {'uid' => 'abcde', 'name' => 'Matt Patterson', 'email' => 'matt@alphagov.co.uk'}
     assert_equal expected, GDS::SSO::User.user_params_from_auth_hash(@auth_hash)
   end
 end
