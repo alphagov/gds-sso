c8340d6b17291353d1863a880d73cf0b2f526762
Matthew Ford <matt@bitzesty.com>
2013-10-04 12:41:30 +0100

    Skip verify_authenticity_token in API controller
    I'm seeing ActionController::InvalidAuthenticityToken when the API
    tries to sync the permissions. So skip the checking.
    
    skip_before_filter is both Rails 3 and 4 compatible.

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -1,4 +1,5 @@
 class Api::UserController < ApplicationController
+  skip_before_filter :verify_authenticity_token
   before_filter :authenticate_user!
   before_filter :require_user_update_permission
 
