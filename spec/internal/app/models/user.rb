09f99542167db66c64b47f7edde0152b9ded1c0a
Tekin Suleyman <tekin@tekin.co.uk>
2014-07-31 16:47:30 +0100

    Don't include attr_accessible in test model
    This already gets set up when GDS::SSO::User is included.

--- spec/internal/app/models/user.rb
+++ spec/internal/app/models/user.rb
@@ -2,6 +2,4 @@ class User < ActiveRecord::Base
   include GDS::SSO::User
 
   serialize :permissions, Array
-
-  attr_accessible :uid, :email, :name, :permissions, :organisation_slug, as: :oauth
 end
