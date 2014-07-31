f744e5f860f0d57e2335995f663490e7d638aa04
Tekin Suleyman <tekin@tekin.co.uk>
2014-07-31 17:55:37 +0100

    Update jenkins.sh to run multi-rails tests

--- jenkins.sh
+++ jenkins.sh
@@ -5,14 +5,14 @@ trap "bundle exec rake signonotron:stop" EXIT
 
 # Gemfile.lock is not in source control because this is a gem
 rm -f Gemfile.lock
+rm -f gemfiles/*.gemfile.lock
 
 # Exclude /tmp from git clean as it only contains the signonotron checkout
 git clean -fdxe /tmp
 
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 
-bundle exec rake signonotron:start
-bundle exec rake spec
+bundle exec rake
 
 if [[ -n "$PUBLISH_GEM" ]]; then
   bundle exec rake publish_gem
