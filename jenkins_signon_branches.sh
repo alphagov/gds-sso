8f7c32e22202e6ef95dd97f85f2320ffe02d7ab4
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-06-12 10:16:15 +0000

    Set the GitHub status context
    This was defaulting to "default" which meant it clashed with the status from
    the signon branch build. By setting the context, GitHub should be able to
    display both statuses.

--- jenkins_signon_branches.sh
+++ jenkins_signon_branches.sh
@@ -9,12 +9,13 @@ VENV_PATH="${HOME}/venv/${JOB_NAME}"
 pip install -q ghtools
 
 REPO="alphagov/signonotron2"
-gh-status "$REPO" "$SIGNON_COMMITISH" pending -d "\"Testing gds-sso against changes #${BUILD_NUMBER} on Jenkins\"" -u "$BUILD_URL" >/dev/null
+GITHUB_STATUS_CONTEXT="Test signon changes against gds-sso master"
+gh-status "$REPO" "$SIGNON_COMMITISH" pending -d "\"Testing gds-sso against changes #${BUILD_NUMBER} on Jenkins\"" -u "$BUILD_URL" -c "$GITHUB_STATUS_CONTEXT" >/dev/null
 
 if ./jenkins.sh; then
-  gh-status "$REPO" "$SIGNON_COMMITISH" success -d "\"Testing gds-sso against changes #${BUILD_NUMBER} succeeded on Jenkins\"" -u "$BUILD_URL" >/dev/null
+  gh-status "$REPO" "$SIGNON_COMMITISH" success -d "\"Testing gds-sso against changes #${BUILD_NUMBER} succeeded on Jenkins\"" -u "$BUILD_URL" -c "$GITHUB_STATUS_CONTEXT" >/dev/null
   exit 0
 else
-  gh-status "$REPO" "$SIGNON_COMMITISH" failure -d "\"Testing gds-sso against changes #${BUILD_NUMBER} failed on Jenkins\"" -u "$BUILD_URL" >/dev/null
+  gh-status "$REPO" "$SIGNON_COMMITISH" failure -d "\"Testing gds-sso against changes #${BUILD_NUMBER} failed on Jenkins\"" -u "$BUILD_URL" -c "$GITHUB_STATUS_CONTEXT" >/dev/null
   exit 1
 fi
