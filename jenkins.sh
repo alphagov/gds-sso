98e98892ed4e5876618d774c9642a18aff257b95
Paul Bowsher <paul.bowsher@gmail.com>
2015-09-04 12:05:23 +0100

    wip ci debug

--- jenkins.sh
+++ jenkins.sh
@@ -15,10 +15,10 @@ git clean -fdxe /tmp
 
 # RBENV_VERSION=1.9.3 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 RBENV_VERSION=2.1 bundle install --path "${HOME}/bundles/${JOB_NAME}"
-RBENV_VERSION=2.2 bundle install --path "${HOME}/bundles/${JOB_NAME}"
+BUNDLE_PATH="${HOME}/bundles/${JOB_NAME}" RBENV_VERSION=2.1 bundle exec rake
 
-RBENV_VERSION=2.1 bundle exec rake
-RBENV_VERSION=2.2 bundle exec rake
+RBENV_VERSION=2.2 bundle install --path "${HOME}/bundles/${JOB_NAME}"
+BUNDLE_PATH="${HOME}/bundles/${JOB_NAME}" RBENV_VERSION=2.2 bundle exec rake
 
 if [[ -n "$PUBLISH_GEM" ]]; then
   bundle exec rake publish_gem
