37379c41d825f0a1bdda09fab4baa839e1f65545
Paul Bowsher <paul.bowsher@gmail.com>
2015-09-04 11:57:13 +0100

    wip ci debug

--- jenkins.sh
+++ jenkins.sh
@@ -13,11 +13,12 @@ rm -f gemfiles/*.gemfile.lock
 # Exclude /tmp from git clean as it only contains the signonotron checkout
 git clean -fdxe /tmp
 
-RBENV_VERSION=1.9.3 bundle install --path "${HOME}/bundles/${JOB_NAME}"
+# RBENV_VERSION=1.9.3 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 RBENV_VERSION=2.1 bundle install --path "${HOME}/bundles/${JOB_NAME}"
+RBENV_VERSION=2.2 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 
-RBENV_VERSION=1.9.3 bundle exec rake
 RBENV_VERSION=2.1 bundle exec rake
+RBENV_VERSION=2.2 bundle exec rake
 
 if [[ -n "$PUBLISH_GEM" ]]; then
   bundle exec rake publish_gem
