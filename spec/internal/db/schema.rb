199018590b316f0d3fe20007bca5b6a110f7de61
Alan Gabbianelli <alan.gabbianelli@digital.cabinet-office.gov.uk>
2020-05-11 11:06:21 +0100

    Fix missing file error with `bundle exec combust`
    After upgrading `combustion` gem from 1.1.2 to 1.3.0 in
    https://github.com/alphagov/gds-sso/pull/219, the test runs started
    failing with this error:
    
    ```
    An error occurred while loading ./spec/controller/api_user_controller_spec.rb.
    Failure/Error: Combustion.initialize! :all
    
    Sprockets::Railtie::ManifestNeededError:
      Expected to find a manifest file in `app/assets/config/manifest.js`
      But did not, please create this file and use it to link any assets that need
      to be rendered by your app:
    
      Example:
        //= link_tree ../images
        //= link_directory ../javascripts .js
        //= link_directory ../stylesheets .css
      and restart your server
    ```
    
    Running `bundle exec combust` creates the missing file.

--- spec/internal/db/schema.rb
+++ spec/internal/db/schema.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 ActiveRecord::Schema.define do
+  # Set up any tables you need to exist for your test suite that don't belong
+  # in migrations.
   create_table "users", :force => true do |t|
     t.string "name",        :null => false
     t.string "uid",         :null => false
