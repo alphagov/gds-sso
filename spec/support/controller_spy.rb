7e81e3d59743280925c46d1df7aa1149584a1c43
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2021-06-10 10:47:31 +0100

    Ignore missing super on ControllerSpy child class

--- spec/support/controller_spy.rb
+++ spec/support/controller_spy.rb
@@ -1,9 +1,10 @@
 class ControllerSpy < ApplicationController
   include GDS::SSO::ControllerMethods
-
+  # rubocop:disable Lint/MissingSuper
   def initialize(current_user)
     @current_user = current_user
   end
+  # rubocop:enable Lint/MissingSuper
 
   def authenticate_user!
     true
