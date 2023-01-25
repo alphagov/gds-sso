3919286ed265def4d95c7665b904c58d6510391f
Kevin Dew <kevindew@me.com>
2023-01-25 15:26:13 +0000

    Remove the end-to-end signon tests and supplementary files
    Following from the last commit which introduced replacement tests, this
    removes the signon integration tests.
    
    These were unusual tests that required a running instance of the signon
    application. These were a complicated coupling that required this gem to
    know a lot about how to set-up signon (including db schema) whcih caused
    a tight coupling. They are difficult and slow to run, and a source of
    surprise for anyone working with this gem.
    
    These tests were only run when gds-sso changed and not when signon
    changed, so any assurances they made about integration would not be
    guaranteed if signon changed later.

--- spec/fixtures/integration/authorize_api_users.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM `oauth_access_tokens`;
-
-INSERT INTO oauth_access_tokens (resource_owner_id, application_id, token, refresh_token, expires_in, created_at)
-  VALUES (1, 1, 'caaeb53be5c7277fb0ef158181bfd1537b57f9e3b83eb795be3cd0af6e118b28', '1bc343797483954d7306d67e96687feccdfdaa8b23ed662ae23e2b03e6661d16', POW(2, 31)-1, '2012-06-27 13:57:47');
-
-INSERT INTO oauth_access_tokens (resource_owner_id, application_id, token, refresh_token, expires_in, created_at)
-  VALUES (1, 2, '98c72f4da02fdc43398e029d05567542944d2a9b0df3c20b0accd8bd6c5dc728', 'e2da0489a58219fd4f542139909737627874ceacd2af23f5c268ccecb36e85af', POW(2, 31)-1, '2014-07-14 09:06:14');
