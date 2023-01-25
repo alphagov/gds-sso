ce63dc2d323265ed3408f0e7c83f013d3bcafaf2
Kevin Dew <kevindew@me.com>
2023-01-25 15:26:13 +0000

    Create a system test for testing a GDS SSO Rails integration
    This is the introduction of RSpec system tests that are intended to
    replace the spec/requests/end_to_end_spec.rb which relies on a running
    instance of Signon.
    
    The motivation to create these new tests is that running the tests that
    rely on a running instance of signon is both difficult and slow (both in
    setup and execution). The cost of the extra effort to deal with these
    exceeds any benefits we have it being a more thorough suite of tests.
    
    These tests aim to match the scenarios we were previously trying to
    cover in the end to end test, but this tries to be more focused on
    explicit scenarios.
    
    To set these tests up I had to turn off Capybara redirect following so
    that Capybara wouldn't try to request a fake signon when served a
    redirect. This did mean some of the tests are quite manual.
    
    In putting this together I discovered that the permission tests didn't
    work correctly so I fixed the code so it could be tested. I also
    discovered that tests were affected by the Rails cache of the temporary
    so I configured that to be a null store.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,22 +2,28 @@
 # Bad things happen if we don't ;-)
 ENV["GDS_SSO_STRATEGY"] = "real"
 
-require "bundler/setup"
-require "combustion"
 require "capybara/rspec"
+require "webmock/rspec"
+require "combustion"
 
-Combustion.initialize! :all
+Combustion.initialize! :all do
+  config.cache_store = :null_store
+end
 
 require "rspec/rails"
 require "capybara/rails"
-require "mechanize"
-require "capybara/mechanize"
+WebMock.disable_net_connect!
 
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].sort.each { |f| require f }
+
+Capybara.register_driver :rack_test do |app|
+  Capybara::RackTest::Driver.new(app, follow_redirects: false)
+end
 
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
+  config.use_transactional_fixtures = true
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
@@ -25,5 +31,6 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 
-  config.include(Warden::Test::Helpers)
+  config.include Warden::Test::Helpers
+  config.include Capybara::DSL
 end
