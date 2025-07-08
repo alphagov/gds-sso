2f411e66e5839d5af97ac5679b97073e9385a0d4
Kevin Dew <kevindew@me.com>
2025-07-08 16:39:47 +0100

    Encapsulate authenticate user code to a method
    This moves the rather basic code for authenticating a user into a
    standalone method. The motivation for this is so that this check can be
    used without logic repetition. The use case I have intended is to use
    this code as part of Rack middleware so that we can authenticate a user
    before running other middleware (Rack::Attack rate limiting middleware).
    
    I made a couple of key implementation decisions here:
    
    1. I chose to add it as a method on GDS::SSO rather than creating a
       dedicated class like AuthoriseUser as it was such trivial code and
       left the door open for further similar utility methods
    2. I went for the slightly unconventional approach of adding a bang
       method without an equivalent without a bang, this seemed appropriate
       as it leaves the door open for an `authenticate_user` method that
       doesn't trigger a failure app if we want to check if a user is
       authenticated without needing them to be authenticated.
    
    This code could potentially be also used in the ControllerMethods
    module, I didn't do this with this change because it doesn't quite map
    correctly with the logic in the controller and looked like it'd be a bit
    messy.

--- spec/unit/authorised_user_constraint_spec.rb
+++ spec/unit/authorised_user_constraint_spec.rb
@@ -3,48 +3,25 @@ require "gds-sso/authorised_user_constraint"
 
 describe GDS::SSO::AuthorisedUserConstraint do
   before do
+    allow(GDS::SSO).to receive(:authenticate_user!).and_return(user)
     allow(GDS::SSO::AuthoriseUser).to receive(:call).and_return(true)
   end
 
   describe "#matches?" do
-    let(:user) { double("user", remotely_signed_out?: remotely_signed_out) }
-    let(:warden) do
-      double(
-        "warden",
-        authenticated?: user_authenticated,
-        user:,
-        authenticate!: nil,
-      )
-    end
-    let(:user_authenticated) { true }
-    let(:remotely_signed_out) { false }
+    let(:user) { TestUser.new }
+    let(:warden) { instance_double("Warden::Proxy") }
     let(:request) { double("request", env: { "warden" => warden }) }
 
-    it "authorises the user" do
-      expect(GDS::SSO::AuthoriseUser).to receive(:call).with(warden.user, %w[signin])
-      expect(warden).not_to receive(:authenticate!)
+    it "authenticates the user" do
+      expect(GDS::SSO).to receive(:authenticate_user!).with(warden)
 
       described_class.new(%w[signin]).matches?(request)
     end
 
-    context "when the user is not authenticated" do
-      let(:user_authenticated) { false }
+    it "authorises the user" do
+      expect(GDS::SSO::AuthoriseUser).to receive(:call).with(user, %w[signin])
 
-      it "authenticates the user" do
-        expect(warden).to receive(:authenticate!)
-
-        described_class.new(%w[signin]).matches?(request)
-      end
-    end
-
-    context "when the user is remotely signed out" do
-      let(:remotely_signed_out) { true }
-
-      it "authenticates the user" do
-        expect(warden).to receive(:authenticate!)
-
-        described_class.new(%w[signin]).matches?(request)
-      end
+      described_class.new(%w[signin]).matches?(request)
     end
   end
 end
